8f884a8aee5eaed10946d279c1141a7179fd1128
Mypos Company <support@mypos.ru>
2024-06-26 16:48:42 +0300

    Init commit

---
+++ Scanner/ScannerInit.java
@@ -0,0 +1,11 @@
+package Scanner;
+
+import java.lang.System;
+import jpos.*;
+import jpos.util.JposPropertiesConst;
+
+public class ScannerInit {
+    public static void Main() {
+        System.out.println("Message");
+    }
+}

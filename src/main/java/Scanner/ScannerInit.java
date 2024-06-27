c855da0f2c2a147ccb32516d86e3c65bc175c9bd
Mypos Company <support@mypos.ru>
2024-06-27 23:40:37 +0300

    creating tasks

--- src/main/java/Scanner/ScannerInit.java
+++ src/main/java/Scanner/ScannerInit.java
@@ -3,16 +3,16 @@ package Scanner;
 import java.lang.System;
 import java.util.Scanner;
 
-import jpos.Scanner;
-import jpos.ScannerConst;
-import jpos.Exception;
+//import jpos.Scanner;
+//import jpos.ScannerConst;
+//import jpos.Exception;
 
 public class ScannerInit {
     public static void main(String[] args) throws InterruptedException {
         try {
-            Scanner scanner = new Scanner();
-        } catch (JposException e) {
-            e.PrintStackTrace();
+          //  Scanner scanner = new Scanner();
+        } catch (/*JPosException e*/Exception ex) {
+         //   e.PrintStackTrace();
         }
     }
 }

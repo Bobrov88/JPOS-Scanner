5b702e39614102af16d7ecf47b3adc3a07fe6113
Mypos Company <support@mypos.ru>
2024-06-27 16:30:47 +0300

    Success first run

--- src/main/java/Scanner/ScannerInit.java
+++ src/main/java/Scanner/ScannerInit.java
@@ -1,12 +1,8 @@
 package Scanner;
 
 import java.lang.System;
-
 public class ScannerInit {
-    public static void Main() throws InterruptedException {
-
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Message");
-        Thread.sleep(5000);
-
     }
 }

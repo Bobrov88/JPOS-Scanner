90d72d865738faabb2e4258cd8684678beb399a6
Mypos Company <support@mypos.ru>
2024-06-27 16:52:54 +0300

    Adding jpos

--- src/main/java/Scanner/ScannerInit.java
+++ src/main/java/Scanner/ScannerInit.java
@@ -1,8 +1,18 @@
 package Scanner;
 
 import java.lang.System;
+import java.util.Scanner;
+
+import jpos.Scanner;
+import jpos.ScannerConst;
+import jpos.Exception;
+
 public class ScannerInit {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Message");
+        try {
+            Scanner scanner = new Scanner();
+        } catch (JposException e) {
+            e.PrintStackTrace();
+        }
     }
 }
